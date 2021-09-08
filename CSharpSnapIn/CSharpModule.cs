a83218cc6b52400936d589a1482ba80f329d63dc
Osazee Isere <oz1.concepts@gmail.com>
2021-09-08 22:28:09 +0100

    initial commit

---
+++ CSharpSnapIn/CSharpModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CommonSnappableTypes;
+namespace CSharpSnapIn
+{
+    [CompanyInfo(CompanyName = "GoSharp Digital Solutons", CompanyUrl = "www.google.com")]
+    public class CSharpModule: IAppFuntionality
+    {
+        void IAppFuntionality.DoIt()
+        {
+            Console.WriteLine("You have just used the C# snap-in!");
+
+        }
+
+    }
+}
